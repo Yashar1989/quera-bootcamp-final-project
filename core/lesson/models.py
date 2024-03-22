3f9ce0aada56723abcd9b5b8b675a37fe05a84fe
hesamgodarzi <hesamgodarzi@gmail.com>
2024-03-22 19:37:47 +0330

    create TermModificationRequest and ReviewRequest and mergencyRemovalRequest

--- core/lesson/models.py
+++ core/lesson/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class TermModificationRequest(models.Model):
+    # student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # added_course = models.ForeignKey(Course, related_name='added_courses', on_delete=models.CASCADE)
+    # removed_course = models.ForeignKey(Course, related_name='removed_courses', on_delete=models.CASCADE)
+    is_approved = models.BooleanField(default=False)
+
+class ReviewRequest(models.Model):
+    # student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    review_text = models.TextField()
+    response = models.TextField(blank=True, null=True)
+
+class EmergencyRemovalRequest(models.Model):
+    # student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    request_result = models.CharField(max_length=50)
+    student_comment = models.TextField()
+    admin_comment = models.TextField()
