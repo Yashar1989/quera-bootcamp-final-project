1fe57fe0802694fff18d9acc3dd71ea46b30e94d
reza miraki <reza.miraki10@gmail.com>
2024-03-24 20:07:27 +0330

    [fix] Assistant CRUD endpoint

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import CreateAssistantAPIView
+from .views import CreateAssistantAPIView, AssistantAPIView
 
 app_name = 'account'
 urlpatterns = [
-    path('assistant/', CreateAssistantAPIView.as_view(), name='assistant'),
+    path('assistant/', CreateAssistantAPIView.as_view(), name='create_assistant'),
+    path('assistant/<uuid:pk>/', AssistantAPIView.as_view(), name='assistant')
 ]
--- core/account/views.py
+++ core/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework import status, serializers
 from rest_framework.response import Response
@@ -11,9 +11,10 @@ from .serializers import AssistantSerializer
 # Create your views here.
 
 
-class CreateAssistantAPIView(CreateAPIView):
+class CreateAssistantAPIView(ListCreateAPIView):
     serializer_class = AssistantSerializer
     permission_classes = [IsAdminUser]
+    queryset = Assistant.objects.all()
 
     def perform_create(self, serializer):
         data = serializer.validated_data
@@ -25,6 +26,28 @@ class CreateAssistantAPIView(CreateAPIView):
     def create(self, request, *args, **kwargs):
         try:
             return super().create(request, *args, **kwargs)
+        except serializers.ValidationError as e:
+            return Response({'error': str(e)}, status=status.HTTP_409_CONFLICT)
+
+        except Exception as e:
+            return super().handle_exception(e)
+
+
+class AssistantAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = AssistantSerializer
+    permission_classes = [IsAdminUser]
+    queryset = Assistant.objects.all()
+
+    def perform_update(self, serializer):
+        data = serializer.validated_data
+        existing_assistant = Assistant.objects.filter(user_id=data['user'])
+        if existing_assistant.exists():
+            raise serializers.ValidationError("An assistant with the same attributes already exists.")
+        serializer.save()
+
+    def update(self, request, *args, **kwargs):
+        try:
+            return super().update(request, *args, **kwargs)
         except serializers.ValidationError as e:
             return Response({'error': str(e)}, status=status.HTTP_409_CONFLICT)
 
