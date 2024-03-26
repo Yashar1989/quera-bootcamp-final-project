845d76c5979f8b45481417179a4134d4289dcb74
reza miraki <reza.miraki10@gmail.com>
2024-03-26 17:09:51 +0330

    fix college models

--- core/college/models.py
+++ core/college/models.py
@@ -1,46 +1,46 @@
 from django.db import models
 from base_files.models import BaseModel
-import uuid
+
+
 # Create your models here.
 
 
-
 class College(BaseModel):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, null=False, blank=False)
 
     def __str__(self):
         return self.name
+
+
+class LearnGroup(BaseModel):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    college = models.ForeignKey(to='College', on_delete=models.CASCADE, related_name='learn_groups')
+
+    def __str__(self):
+        return self.name + self.college.name
 
 
 class Field(BaseModel):
-    DEGREES_CHOICES=[
-        ('advanced_diploma' ,'Advanced Diploma'),
-        ('bachelor' ,'Bachelor'),
-        ('master' ,'Master'),
-        ('doctorate' ,'Doctorate'),
-        ('post_doctorate' ,'Post Doctorate'),
+    DEGREES_CHOICES = [
+        ('advanced_diploma', 'Advanced Diploma'),
+        ('bachelor', 'Bachelor'),
+        ('master', 'Master'),
+        ('doctorate', 'Doctorate'),
+        ('post_doctorate', 'Post Doctorate'),
     ]
-    name = models.CharField(max_length=250)
-    leran_group = models.CharField(max_length=250)
-    number_unit = models.IntegerField()
-    degrees = models.CharField(max_length=50,choices=DEGREES_CHOICES)
-    
+    name = models.CharField(max_length=255, null=False, blank=False)
+    learn_group = models.ForeignKey(to='LearnGroup', on_delete=models.CASCADE, related_name='fields')
+    unit_number = models.PositiveIntegerField()
+    degrees = models.CharField(max_length=50, choices=DEGREES_CHOICES)
+
     def __str__(self):
         return self.name
- 
+
 
 class SelectUnit(BaseModel):
     applicant_student = models.ForeignKey(to='account.Student', on_delete=models.CASCADE)
     on_demand_courses = models.ForeignKey(to='lesson.Lesson', on_delete=models.CASCADE)
     apporoval_student = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.name
 
-
-class LeranGroup(BaseModel):
-    name = models.CharField(max_length=250)
-    subgroup = models.ForeignKey(to='self', on_delete=models.CASCADE)
-    
     def __str__(self):
         return self.name
